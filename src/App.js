637537f2e7dd0f1a195c36e18ff5459904bee498
Waynetron <wpetzler@gmail.com>
2020-02-14 17:16:15 +1100

    Improved visuals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const CELL_SIZE = 30;
+
+const emptyRoom = [
+  ['#','#','#','#','#','#','#','#'],
+  ['#','.','.','.','.','.','.','#'],
+  ['#','.','.','.','.','.','.','#'],
+  ['#','.','.','.','.','.','.','#'],
+  ['#','.','.','.','.','.','.','#'],
+  ['#','.','.','.','.','.','.','#'],
+  ['#','.','.','.','.','.','.','#'],
+  ['#','#','#','#','#','#','#','#'],
+];
+
+const cellStyle = (x, y, char) => {
+  return {
+    position: 'relative',
+    left: x * CELL_SIZE,
+    top: y * CELL_SIZE,
+    width: 0,
+    height: 0,
+  }
+};
+
+const cellInnerStyle = (x, y, char) => {
+  const charColourMap = {
+    ['#']: '#AAA',
+    ['.']: '#333'
+  };
+
+  return {
+  width: CELL_SIZE,
+  height: CELL_SIZE,
+  color: charColourMap[char] || '#AAA',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  
+}};
+
+const mapStyle = (map) => ({
+  width: map.length * CELL_SIZE,
+  height: map[0].length * CELL_SIZE
+});
+
 function App() {
+  const move = ({x, y}) => {
+    console.log(x, y);
+  }
+  
+  const handleKeyDown = ({key}) => {
+    const directionMappings = {
+      ArrowUp: {x: 0, y: -1},
+      ArrowDown: {x: 0, y: 1},
+      ArrowLeft: {x: -1, y: 0},
+      ArrowRight: {x: 1, y: 0}
+    }
+  
+    if (directionMappings[key]) {
+      move(directionMappings[key]);
+    }
+  }
+
+  const addPlayer = (map) => {
+    map[1][1] = '@';
+    return map;
+  }
+  
+  const [player, setPlayer] = useState({position: {x: 1, y: 1}})
+  const [map, setMap] = useState(addPlayer(emptyRoom))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div
+        className="map"
+        style={mapStyle(map)}
+        onKeyDown={handleKeyDown}
+        tabIndex={0}
+      >
+        {map.map((row, y) =>
+          row.map((char, x) =>
+            <div className="cell" style={cellStyle(x, y, char)}>
+              <div style={cellInnerStyle(x, y, char)}>
+                <p>{char}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 }
